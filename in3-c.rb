131186f047942da8b9aa974f3e64c349f34eacd4
pari <parithosh@slock.it>
2019-09-24 12:02:31 +0200

    initial

---
+++ in3-c.rb
@@ -0,0 +1,12 @@
+class In3C < Formula
+  desc "C-version of the in3 client "
+  homepage ""
+  url "https://github.com/slockit/in3-c/archive/v1.0.0.tar.gz"
+  sha256 "2c6ae59cd698706416dfee8cb762670fe0bf3caab6d91d24bb02c5d217978be1"
+
+
+  def install
+	./build_release.sh
+  end
+
+end
