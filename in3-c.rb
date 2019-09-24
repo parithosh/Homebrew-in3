03189859ae3ef868869ca3372baa77c7be8d3b5f
pari <parithosh@slock.it>
2019-09-24 13:48:53 +0200

    changed url

--- in3-c.rb
+++ in3-c.rb
@@ -1,13 +1,13 @@
 class In3C < Formula
   desc "C-version of the in3 client "
   homepage ""
-  url "https://github.com/slockit/in3-c/archive/v1.0.0.tar.gz"
+  url "https://github.com/slockit/in3-c/releases/download/v1.0.0/in3-1.0.0-mac.zip"
   sha256 "2c6ae59cd698706416dfee8cb762670fe0bf3caab6d91d24bb02c5d217978be1"
 
 
   def install
-	system "cd","scripts"
-	bin.install "build_release.sh"
+	system "cd","bin"
+	bin.install "in3"
   end
 
 end
