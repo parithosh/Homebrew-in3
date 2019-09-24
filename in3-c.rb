f9a6f4bae28256bf14a6b4a6b7a0f43962253754
pari <parithosh@slock.it>
2019-09-24 13:51:03 +0200

    changed hash

--- in3-c.rb
+++ in3-c.rb
@@ -2,7 +2,7 @@ class In3C < Formula
   desc "C-version of the in3 client "
   homepage ""
   url "https://github.com/slockit/in3-c/releases/download/v1.0.0/in3-1.0.0-mac.zip"
-  sha256 "2c6ae59cd698706416dfee8cb762670fe0bf3caab6d91d24bb02c5d217978be1"
+  sha256 "efe56616fb82a2660ddc928ce2451033f1483bff51f7bcc76eaf4b18df0be0cb"
 
 
   def install
